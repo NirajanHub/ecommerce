8280e2a7416e2f84a2e0922e73b2d7eee6debb90
nirajanchapagain <nirajanc19@gmail.com>
2024-01-28 13:52:48 -0800

    done

--- app/src/main/java/com/ncodes/ecommerce/landing/presentation/LandingScreen.kt
+++ app/src/main/java/com/ncodes/ecommerce/landing/presentation/LandingScreen.kt
@@ -72,11 +72,6 @@ fun LandingScreen(
                         route = Screens.LoginNav.route,
                         icon = Icons.Sharp.AccountBox,
                         badgeCount = 1
-                    ),   BottomNavItems(
-                        "home",
-                        route = Screens.LoginNav.route,
-                        icon = Icons.Sharp.AccountBox,
-                        badgeCount = 1
                     ),
                 ),
                 navController = navController!!,
