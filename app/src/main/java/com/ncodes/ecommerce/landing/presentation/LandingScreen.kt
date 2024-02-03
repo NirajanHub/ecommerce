02901c3f8cd90b4173d65c71d51d02b595bcd684
nirajanchapagain <nirajanc19@gmail.com>
2024-02-02 18:37:18 -0800

    added internal_qa

--- app/src/main/java/com/ncodes/ecommerce/landing/presentation/LandingScreen.kt
+++ app/src/main/java/com/ncodes/ecommerce/landing/presentation/LandingScreen.kt
@@ -15,11 +15,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -85,7 +83,6 @@ fun LandingScreen(
         Surface(
             modifier = Modifier
                 .fillMaxSize()
-
                 .padding(it),
             color = MaterialTheme.colorScheme.background
         ) {
