616aa85be87593f379f08fd74923fa6a7e67f345
nirajanchapagain <nirajanc19@gmail.com>
2024-02-26 14:02:09 -0800

    trying to update directly to release try-1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.ncodes.ecommerce"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "2.2"
+        versionCode = 5
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
